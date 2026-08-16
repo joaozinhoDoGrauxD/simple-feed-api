91da4a482ca7ed01170ced024250acf4a293b8a7
joaozinhoDoGrauXd <joaoninguem1301@gmail.com>
2026-08-16 13:37:17 -0300

    feat: dnsHandler

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,7 +1,12 @@
 import { DataSource } from "typeorm";
+import {Resolver} from "node:dns"
 import { User } from "../entities/User";
 import { List } from "../entities/List";
 import { Bookmark } from "../entities/Bookmark";
+
+const resolver = new Resolver()
+
+resolver.setServers(['1.1.1.1', '8.8.8.8'])
 
 const setupDBName = () : string => {
     if(process.env.BUN_ENV === 'production'){
